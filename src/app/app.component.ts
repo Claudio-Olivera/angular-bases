61f6a398c9b618276971dc67f503e9bc39ec38e3
Claudio-Olivera <claudioolivera753@gmail.com>
2023-07-01 01:56:02 -0300

    revert-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
-  title = 'gifs-app';
-=======
 
   public title:string = 'Mi primera app de Angular';
 
->>>>>>> 9ba083ad2b816e89c02b5fed685c7cb909bd87e8
 }
